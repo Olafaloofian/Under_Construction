0299ff525964e0fcc5d1c7e021e2b23906fc89fc
olafaloofian <bulluffalo@gmail.com>
2019-07-22 14:32:50 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+let xPosition = 0
+let yPosition = 0
+let screenWidth = null
+let screenHeight = null
+
+const getMousePosition = (e) => {
+    xPosition = e.clientX
+    yPosition = e.clientY
+    const followElement = document.getElementById('follow')
+    followElement.style.top = (yPosition - 35) + 'px'
+    followElement.style.left = (xPosition - 35) + 'px'
+    document.getElementById('x-position').innerText = xPosition
+    document.getElementById('y-position').innerText = `${screenHeight - yPosition}`
+    
+
+}
+
+const getScreenSize = (e) => {
+    screenWidth = window.innerWidth
+    screenHeight = window.innerHeight
+    document.getElementById('width').innerText = `${screenWidth}px`
+    document.getElementById('height').innerText = `${screenHeight}px`
+}
+
+window.addEventListener('load', getScreenSize)
+window.addEventListener('resize', getScreenSize)
+window.addEventListener('mousemove', getMousePosition)
